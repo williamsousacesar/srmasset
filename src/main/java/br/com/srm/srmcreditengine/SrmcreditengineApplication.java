49e54b6ba2f80cd218da297ec14569346be872a0
williamsousacesar <william.cesar@gmail.com>
2026-08-19 23:39:07 +0000

    Projeto inicial criado via Spring Initializr.

---
+++ src/main/java/br/com/srm/srmcreditengine/SrmcreditengineApplication.java
@@ -0,0 +1,13 @@
+package br.com.srm.srmcreditengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmcreditengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrmcreditengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/srm/srmcreditengine/SrmcreditengineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.srm.srmcreditengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmcreditengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
